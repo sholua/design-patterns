6b41eb0b39b4c26f091636fb510dc3d291bb9a9f
Oleksandr Shevchuk <oleksandr.shevchuk@lanars.com>
2022-11-07 14:42:50 +0200

    Singleton

---
+++ creational/singleton.js
@@ -0,0 +1,24 @@
+const Singleton = (() => {
+  let course;
+
+  const assignCourse = () => {
+    const course = new Object({ name: "JavaScript" });
+    return course;
+  };
+
+  return {
+    getInstance: () => {
+      if (!course) {
+        course = assignCourse();
+      }
+      return course;
+    },
+  };
+})();
+
+const buyFirstTime = Singleton.getInstance();
+const buySecondTime = Singleton.getInstance();
+
+if (buyFirstTime === buySecondTime) {
+  console.log("Go to Course");
+}
