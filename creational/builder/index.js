2dd773b65afbaad982307cdcda2c42928e09da3f
Oleksandr Shevchuk <oleksandr.shevchuk@lanars.com>
2022-11-07 15:18:00 +0200

    Builder

---
+++ creational/builder/index.js
@@ -0,0 +1,14 @@
+const CourseBuilder = require("./CourseBuilder");
+
+// Bad approach with "magic" params
+//const course_1 = new CourseBuilder('Design Patterns 1', 0, true, 149 , true);
+//const course_2 = new CourseBuilder('Design Patterns 1', 0,false, 0, false);
+
+const course_1 = new CourseBuilder("Design Patterns 1")
+  .makePaid(100)
+  .makeCampain()
+  .build();
+const course_2 = new CourseBuilder("Design Patterns 2").build();
+
+course_1.toString();
+course_2.toString();
